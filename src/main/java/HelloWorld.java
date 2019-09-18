fad7ef5043c00cf549a79dd20f960335a660990c
faizan <ffaaiizzuu@gmail.com>
2019-09-18 18:47:36 +0530

    update1111

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Say hello to Devops");
+		get("/hello", (req, res) -> "Hello World");
 	}
 }
